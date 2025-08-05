a10bc29efa95ed276b1b978b765fca3ff6e9c691
HaJincheol <smilecjf@naver.com>
2025-08-05 13:14:34 +0000

    Add NtruKeyswitchKey

--- tfhe/src/ntru/entities/mod.rs
+++ tfhe/src/ntru/entities/mod.rs
@@ -3,9 +3,11 @@ pub mod ntru_ciphertext;
 pub mod ntru_ciphertext_list;
 pub mod ngsw_ciphertext;
 pub mod fourier_ngsw_ciphertext;
+pub mod ntru_keyswitch_key;
 
 pub use ntru_secret_key::*;
 pub use ntru_ciphertext::*;
 pub use ntru_ciphertext_list::*;
 pub use ngsw_ciphertext::*;
 pub use fourier_ngsw_ciphertext::*;
+pub use ntru_keyswitch_key::*;
